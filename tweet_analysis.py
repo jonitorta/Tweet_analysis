d288c284565a7a4185e5b3b62d13c7d17346dbab
jonitorta <joni50@live.com.mx>
2022-05-08 16:21:32 -0500

    Se cambio el filtro de palabras usando la lista de nltk mejorando los resultados cuando filtramos.

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -1,15 +1,22 @@
 #Useful libraries we need.
-from copy import copy
 from datetime import date, datetime
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import numpy as np
 from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+#nltk.download('stopwords')
+
+
+extra_stop_words = ["no","si","así","hacer","cosas","ver","voy","va","puedes","luego",
+                    "ser","hecho","hace","tener","sé","mejor","dicho"]
+stopWords = stopwords.words("spanish") + extra_stop_words
 
 #Select twitter querry we want to obtain tweets.
-querry = "(from:@EvilAFM)"
+querry = "(from:@titodeskanar)"
 #If enough tweets are found program will collect at max limit number.
-limit = 50
+limit = 500
 
 
 
@@ -36,20 +43,22 @@ class Tweet_analysis() :
     
     #Method to obtain # of most common words from the tweets of the querry, len_filter
     #filter the words with less or equal number of letters.
-    def common_words(self, number = 10, len_filter = 3):
+    def common_words(self, number = 10):
         tweet_longer = ""
-        for t in self.content:
-            tweet_longer +=t 
+        f_tweet_longer = []
+        for t in df["Tweet"]: tweet_longer +=t
+        tweet_longer=tweet_longer.lower()
         count = Counter(tweet_longer.split(" "))
         lista, diccionario = list(count) , dict(count) 
-        index_short_element = np.where( (np.array ([len(element) for element in lista] ) )<=len_filter )[0]
-        for index in index_short_element :
-            diccionario.pop( lista[index] )
-        count = Counter(diccionario).most_common(number)
-        return dict(count)
+        for w in lista :
+            if w not in stopWords :
+                f_tweet_longer.append(w)
+        times_word_appear = [diccionario[j] for j in f_tweet_longer]
+        common_words = Counter(dict(zip(f_tweet_longer,times_word_appear  ))).most_common(number)
+        return common_words
 
 #Count tweets per day
-    def tweets_per_day(self,last_n_days = 30) :
+    def tweets_per_day(self, last_n_days = 30 ) :
         copy_df = self.geneal_info
         copy_df["Date"] = [fechas.to_pydatetime().date() for fechas in self.geneal_info["Date"]]
         tweets_per_day = copy_df.groupby("Date").count()["Tweet"]
@@ -58,19 +67,17 @@ class Tweet_analysis() :
 
 
 
-#tweets = []
-#for tweet in sntwitter.TwitterSearchScraper(querry).get_items():
-#    if len (tweets) == limit : break
-#    else : tweets.append([ tweet.date, tweet.user , tweet.content ])
-#df = pd.DataFrame( tweets, columns=["Date", "User", "Tweet"] )
+tweets = []
+for tweet in sntwitter.TwitterSearchScraper(querry).get_items():
+    if len (tweets) == limit : break
+    else : tweets.append([ tweet.date, tweet.user , tweet.content ])
+df = pd.DataFrame( tweets, columns=["Date", "User", "Tweet"] )
 
-#copy_df = df
-#copy_df["Date"] = [fechas.to_pydatetime().date() for fechas in df["Date"]]
-#tweets_per_day = copy_df.groupby("Date").count()["Tweet"]
-#print(tweets_per_day[0:30])
 
-#Dates = [fechas.to_pydatetime().date() for fechas in df["Date"]]
-#copy_df["Date"] = Dates
+
+
+
+
 
 
 
@@ -79,7 +86,8 @@ class Tweet_analysis() :
 
 
 User = Tweet_analysis(querry=querry)
-print(User.tweets_per_day())
+print(User.common_words(20))
+
 
 
 pass
