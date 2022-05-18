0a7d857c5ae61639fb60d12b3d43f52e038ebf36
jonitorta <joni50@live.com.mx>
2022-05-18 15:31:25 -0500

    Cambio la lista de atributos extra del usuario a este archivo.

--- config_file.py
+++ config_file.py
@@ -4,7 +4,12 @@ options = {
     "limit": 60,
     "user info" : False,
     "extra stop words spanish" :  ["no","si","así","hacer","cosas","ver","voy","va","puedes","luego",
-                    "ser","hecho","hace","tener","sé","mejor","dicho"]
+                    "ser","hecho","hace","tener","sé","mejor","dicho"],
+    "user info attributes" : ["username", "id", "displayname", "description",
+			"rawDescription", "descriptionUrls", "created", "followersCount", "friendsCount", 
+			"statusesCount", "favouritesCount", "listedCount", "mediaCount","location",
+			"protected", "linkUrl","linkTcourl", "profileImageUrl", 
+			"profileBannerUrl", "label"]
 
 
 }
