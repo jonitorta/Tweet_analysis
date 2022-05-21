348d23716350741bf34c58fec7133ace84761cc2
jonitorta <joni50@live.com.mx>
2022-05-21 11:54:39 -0500

    Quito opciones para info extra del usuario

--- config_file.py
+++ config_file.py
@@ -3,18 +3,13 @@ options = {
     
     "querry": "medicos, mexico lang:es",
     
-    "limit": 10,
+    "limit": 10000,
     
-    "user info" : False,
+   
     
     "extra stop words spanish" :  ["no","si","así","hacer","cosas","ver","voy","va","puedes","luego",
                     "ser","hecho","hace","tener","sé","mejor","dicho"],
     
-    "user info attributes" : ["username", "id", "displayname", "description",
-			"rawDescription", "descriptionUrls", "created", "followersCount", "friendsCount", 
-			"statusesCount", "favouritesCount", "listedCount", "mediaCount","location",
-			"protected", "linkUrl","linkTcourl", "profileImageUrl", 
-			"profileBannerUrl", "label"],
     "save status" : True,
 
     "file name" : "tweets_info.pkl",
