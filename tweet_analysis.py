cc4d0720ba22d3faa8e9032b581d99aac94e4852
Jonathan_Axel <joni50@live.com.mx>
2022-05-20 20:07:18 -0500

    Agrego variable tweets_per_user que cuenta tweets por usuario

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -11,6 +11,7 @@ user_info = options["user info"]
 user_info_attributes = options["user info attributes"]
 save_status = options["save status"]
 file_name = options["file name"]
+commond_words_file = options["commond_word_file_name"]
 
 if not path.exists(file_name):
     get_data = Tweet_analysis( querry= querry, limit=limit,
@@ -18,13 +19,16 @@ if not path.exists(file_name):
                            user_info_attributes= user_info_attributes,
                            save_data=save_status,
                            file_name= file_name)
+    
     commond_words = get_data.common_words(100)
-    with open ("text_file.txt","w") as f:
+    with open (commond_words_file,"w",encoding='utf-8') as f:
         for words in commond_words : 
             f.write(str(words)+"\n")
     
 
 data = pd.read_pickle(file_name)
+data.info()
+tweets_per_user = data.groupby("User").count()["Tweet"].sort_values( ascending = False )
 
 
 pass
