20fe69ac046e03d318a97e193eb8e11f8e9be85f
Jonathan_Axel <joni50@live.com.mx>
2022-05-20 19:49:09 -0500

    Agrego código para guardar las palabras mas utilizadas en la querry en un archivo de texto

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -1,6 +1,9 @@
+import pandas as pd
+import numpy as np
 from tweet_scrapper import Tweet_analysis
 from config_file import options
 from os import path
+
 
 querry = options["querry"]
 limit = options["limit"]
@@ -15,3 +18,13 @@ if not path.exists(file_name):
                            user_info_attributes= user_info_attributes,
                            save_data=save_status,
                            file_name= file_name)
+    commond_words = get_data.common_words(100)
+    with open ("text_file.txt","w") as f:
+        for words in commond_words : 
+            f.write(str(words)+"\n")
+    
+
+data = pd.read_pickle(file_name)
+
+
+pass
