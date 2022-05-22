5bb07118aecd56c4c54a46b4e642aa289ee6cade
Jonathan_Axel <joni50@live.com.mx>
2022-05-21 23:46:56 -0500

    Quito opciones de info extra del usuario

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -7,18 +7,14 @@ from os import path
 
 querry = options["querry"]
 limit = options["limit"]
-user_info = options["user info"]
-user_info_attributes = options["user info attributes"]
 save_status = options["save status"]
 file_name = options["file name"]
 commond_words_file = options["commond_word_file_name"]
 
 if not path.exists(file_name):
-    get_data = Tweet_analysis( querry= querry, limit=limit,
-                           user_info=user_info, 
-                           user_info_attributes= user_info_attributes,
-                           save_data=save_status,
-                           file_name= file_name)
+    get_data = Tweet_analysis(  querry= querry, limit=limit,
+                                save_data=save_status,
+                                file_name= file_name )
     
     commond_words = get_data.common_words(100)
     with open (commond_words_file,"w",encoding='utf-8') as f:
